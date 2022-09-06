e5ddbf44985832501b6a40a2c31d9a2b470d7b6e
Arnau Orriols <arnauorriolsmiro@gmail.com>
2022-09-06 10:28:20 +0200

    Remove old `n` option in bench calls, and ensure async ops are awaited

--- cli/bench/deno_common.js
+++ cli/bench/deno_common.js
@@ -1,7 +1,7 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
 // v8 builtin that's close to the upper bound non-NOPs
-Deno.bench("date_now", { n: 5e5 }, () => {
+Deno.bench("date_now", () => {
   Date.now();
 });
 
@@ -33,12 +33,11 @@ Deno.bench("op_void_sync", () => sync());
 
 Deno.bench(
   "op_void_async",
-  { n: 1e6 },
-  () => Deno.core.opAsync("op_void_async"),
+  async () => Deno.core.opAsync("op_void_async"),
 );
 
 // A very lightweight op, that should be highly optimizable
-Deno.bench("perf_now", { n: 5e5 }, () => {
+Deno.bench("perf_now", () => {
   performance.now();
 });
 
@@ -51,29 +50,29 @@ Deno.bench("open_file_sync", () => {
 // also a decent representation of a non-trivial JSON-op
 {
   let i = 0;
-  Deno.bench("url_parse", { n: 5e4 }, () => {
+  Deno.bench("url_parse", () => {
     new URL(`http://www.google.com/${i}`);
     i++;
   });
 }
 
-Deno.bench("blob_text_large", { n: 100 }, () => {
+Deno.bench("blob_text_large", () => {
   new Blob([input]).text();
 });
 
 const input = "long-string".repeat(99999);
-Deno.bench("b64_rt_long", { n: 100 }, () => {
+Deno.bench("b64_rt_long", () => {
   atob(btoa(input));
 });
 
-Deno.bench("b64_rt_short", { n: 1e6 }, () => {
+Deno.bench("b64_rt_short", () => {
   atob(btoa("123"));
 });
 
 {
   const buf = new Uint8Array(100);
   const file = Deno.openSync("/dev/zero");
-  Deno.bench("read_zero", { n: 5e5 }, () => {
+  Deno.bench("read_zero", () => {
     Deno.readSync(file.rid, buf);
   });
 }
@@ -82,15 +81,14 @@ Deno.bench("b64_rt_short", { n: 1e6 }, () => {
   // Not too large since we want to measure op-overhead not sys IO
   const dataChunk = new Uint8Array(100);
   const file = Deno.openSync("/dev/null", { write: true });
-  Deno.bench("write_null", { n: 5e5 }, () => {
+  Deno.bench("write_null", () => {
     Deno.writeSync(file.rid, dataChunk);
   });
 }
 
 Deno.bench(
   "read_128k",
-  { n: 5e4 },
-  () => Deno.readFile("./cli/bench/testdata/128k.bin"),
+  async () => Deno.readFile("./cli/bench/testdata/128k.bin"),
 );
 
-Deno.bench("request_new", { n: 5e5 }, () => new Request("https://deno.land"));
+Deno.bench("request_new", () => new Request("https://deno.land"));
